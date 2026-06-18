5d3e6b28b36470a3ffd28c9bf1dab018eb2694ee
codewave <tobeadigwe3000@gmail.com>
2026-06-18 14:24:41 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  base: '/my-e-commerce-website/', 
-  plugins: [
-    react(),
-    tailwindcss(),
+export default defineConfig( {
+    base: '/my-e-commerce-website/', 
+    plugins: [
+      react(),
+      tailwindcss(),
   ],
-  
 });
 
